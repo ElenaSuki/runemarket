d5cda4ee5edd3067dc45624c9a2493b779b7cc71
Gin Miraing <ginmiraing@outlook.com>
2024-05-24 23:55:35 +0800

    feat: add unisat api key

--- app/lib/apis/unisat/api.ts
+++ app/lib/apis/unisat/api.ts
@@ -1,56 +1,122 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import { Network, networks } from "bitcoinjs-lib";
 
 import RedisInstance from "@/lib/server/redis.server";
 import { AddressRuneAsset } from "@/lib/types/rune";
 
 import {
+  AddressRunesBalanceReq,
   AddressRunesUTXOReq,
   RunesInfoReq,
   UnisatInscriptionInfoType,
 } from "./type";
 
-const API_KEY =
-  "59847a3e55dce7fe73c80877ae2b2bc5e9eb2e797de9e5c4f6d602dc996cf706";
+const API_KEY_LIST = [
+  "59847a3e55dce7fe73c80877ae2b2bc5e9eb2e797de9e5c4f6d602dc996cf706",
+  "129ba06ce6cc2ba1bd771c6444ad632e8480b0d31fb87a326a2592fd8e0b958e",
+  "55a80b1b4eec7afd00d00d955681b87014796c34856ff749a052695d05f17a5e",
+];
 
 const BaseUrl = (network: Network) =>
   network === networks.testnet
     ? "https://open-api-testnet.unisat.io"
     : "https://open-api.unisat.io";
 
-const AxiosInstance = axios.create({
-  timeout: 1000 * 20,
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-  },
-});
+export const tryWithApiKey = async <T>(
+  network: Network,
+  url: string,
+  params?: any,
+) => {
+  for (const key of API_KEY_LIST) {
+    try {
+      const resp = await axios.get<{
+        code: number;
+        msg: string;
+        data: T;
+      }>(`${BaseUrl(network)}${url}`, {
+        headers: {
+          Authorization: `Bearer ${key}`,
+        },
+        params,
+      });
 
-export const getRuneInfo = async (network: Network, runeId: string) => {
+      if (resp.data.code !== 0) {
+        throw new Error(resp.data.msg);
+      }
+
+      return resp.data.data;
+    } catch (e) {
+      console.log(e);
+
+      if (isAxiosError(e) && e.response?.status === 403) {
+        throw new Error(e.response?.data?.data?.msg || "Unknow error");
+      }
+    }
+  }
+
+  throw new Error("All API keys are failed");
+};
+
+export const getRuneInfo = async (
+  network: Network,
+  runeId: string,
+): Promise<RunesInfoReq> => {
   const cache = await RedisInstance.get(`unisat:rune:info:${runeId}`);
 
   if (cache) {
     return JSON.parse(cache);
   }
 
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: RunesInfoReq | null;
-  }>(`${BaseUrl(network)}/v1/indexer/runes/${runeId}/info`);
+  const data = await tryWithApiKey<RunesInfoReq | null>(
+    network,
+    `/v1/indexer/runes/${runeId}/info`,
+  );
 
-  if (!resp.data.data) {
+  if (!data) {
     throw new Error("Rune not found");
   }
 
   await RedisInstance.set(
     `unisat:rune:info:${runeId}`,
-    JSON.stringify(resp.data.data),
+    JSON.stringify(data),
     "EX",
     60 * 60,
     "NX",
   );
 
-  return resp.data.data;
+  return data;
+};
+
+export const getAddressRuneBalanceList = async (
+  network: Network,
+  address: string,
+): Promise<AddressRunesBalanceReq[]> => {
+  const cache = await RedisInstance.get(
+    `unisat:address:rune:balance:${address}`,
+  );
+
+  if (cache) {
+    return JSON.parse(cache);
+  }
+
+  const data = await tryWithApiKey<{
+    detail: AddressRunesBalanceReq[];
+    start: number;
+    total: number;
+  }>(network, `/v1/indexer/address/${address}/runes/balance-list`, {
+    start: 0,
+    limit: 500,
+  });
+
+  await RedisInstance.set(
+    `unisat:address:rune:balance:${address}`,
+    JSON.stringify(data.detail),
+    "EX",
+    60 * 2,
+    "NX",
+  );
+
+  return data.detail;
 };
 
 export const getAddressRuneUTXOsByUnisat = async (
@@ -66,22 +132,16 @@ export const getAddressRuneUTXOsByUnisat = async (
     return JSON.parse(cache);
   }
 
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: {
-      start: number;
-      total: number;
-      utxo: AddressRunesUTXOReq[];
-    };
-  }>(`${BaseUrl(network)}/v1/indexer/address/${address}/runes/${runeId}/utxo`, {
-    params: {
-      start: 0,
-      limit: 500,
-    },
+  const data = await tryWithApiKey<{
+    utxo: AddressRunesUTXOReq[];
+    start: number;
+    total: number;
+  }>(network, `/v1/indexer/address/${address}/runes/${runeId}/utxo`, {
+    start: 0,
+    limit: 500,
   });
 
-  const array = resp.data.data.utxo.map((utxo) => ({
+  const array = data.utxo.map((utxo) => ({
     txid: utxo.txid,
     vout: utxo.vout,
     value: utxo.satoshi,
@@ -110,78 +170,31 @@ export const getAddressRuneUTXOsByUnisat = async (
 };
 
 export const getBTCUTXOs = async (network: Network, address: string) => {
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: {
-      utxo: {
-        txid: string;
-        vout: number;
-        satoshi: number;
-        scriptType: string;
-        scriptPk: string;
-        codeType: number;
-        address: string;
-        height: number;
-        idx: number;
-        isOpInRBF: boolean;
-        isSpent: boolean;
-        inscriptions: {
-          inscriptionId: string;
-          isBRC20: boolean;
-          moved: boolean;
-        }[];
+  const data = await tryWithApiKey<{
+    utxo: {
+      txid: string;
+      vout: number;
+      satoshi: number;
+      scriptType: string;
+      scriptPk: string;
+      codeType: number;
+      address: string;
+      height: number;
+      idx: number;
+      isOpInRBF: boolean;
+      isSpent: boolean;
+      inscriptions: {
+        inscriptionId: string;
+        isBRC20: boolean;
+        moved: boolean;
       }[];
-    };
-  }>(`${BaseUrl(network)}/v1/indexer/address/${address}/utxo-data`, {
-    params: {
-      cursor: 0,
-      size: 1000,
-    },
+    }[];
+  }>(network, `/v1/indexer/address/${address}/utxo-data`, {
+    cursor: 0,
+    size: 1000,
   });
 
-  return resp.data.data.utxo;
-};
-
-export const checkUTXOBalance = async (
-  network: Network,
-  txid: string,
-  index: number,
-): Promise<
-  {
-    runeId: string;
-    rune: string;
-    symbol: string;
-    spacedRune: string;
-    amount: string;
-    divisibility: number;
-  }[]
-> => {
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: {
-      rune: string;
-      runeid: string;
-      amount: string;
-      divisibility: number;
-      symbol: string;
-      spacedRune: string;
-    }[];
-  }>(`${BaseUrl(network)}/v1/indexer/runes/utxo/${txid}/${index}/balance`);
-
-  if (resp.data.data.length === 0) return [];
-
-  const array = resp.data.data.map((rune) => ({
-    runeId: rune.runeid,
-    rune: rune.rune,
-    symbol: rune.symbol,
-    spacedRune: rune.spacedRune,
-    amount: rune.amount,
-    divisibility: rune.divisibility,
-  }));
-
-  return array;
+  return data.utxo;
 };
 
 export const getAddressInscriptions = async (
@@ -206,26 +219,22 @@ export const getAddressInscriptions = async (
     return JSON.parse(cache);
   }
 
-  const resp = await AxiosInstance.get<{
-    data: {
-      inscription: {
-        utxo: {
-          txid: string;
-          vout: number;
-          satoshi: number;
-          isSpent: boolean;
-        };
-        inscriptionId: string;
-      }[];
-    };
-  }>(`${BaseUrl(network)}/v1/indexer/address/${address}/inscription-data`, {
-    params: {
-      cursor: 0,
-      size: 1000,
-    },
+  const data = await tryWithApiKey<{
+    inscription: {
+      utxo: {
+        txid: string;
+        vout: number;
+        satoshi: number;
+        isSpent: boolean;
+      };
+      inscriptionId: string;
+    }[];
+  }>(network, `/v1/indexer/address/${address}/inscription-data`, {
+    cursor: 0,
+    size: 1000,
   });
 
-  const array = resp.data.data.inscription.filter(
+  const array = data.inscription.filter(
     (inscription) => !inscription.utxo.isSpent,
   );
 
@@ -244,17 +253,32 @@ export const getInscriptionInfo = async (
   network: Network,
   inscriptionId: string,
 ): Promise<UnisatInscriptionInfoType> => {
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: UnisatInscriptionInfoType;
-  }>(`${BaseUrl(network)}/v1/indexer/inscription/info/${inscriptionId}`);
+  const cache = await RedisInstance.get(
+    `unisat:inscription:info:${inscriptionId}`,
+  );
 
-  if (!resp.data.data) {
+  if (cache) {
+    return JSON.parse(cache);
+  }
+
+  const data = await tryWithApiKey<UnisatInscriptionInfoType | null>(
+    network,
+    `/v1/indexer/inscription/info/${inscriptionId}`,
+  );
+
+  if (!data) {
     throw new Error("Inscription not found");
   }
 
-  return resp.data.data;
+  await RedisInstance.set(
+    `unisat:inscription:info:${inscriptionId}`,
+    JSON.stringify(data),
+    "EX",
+    60 * 5,
+    "NX",
+  );
+
+  return data;
 };
 
 export const getRuneHolders = async (
@@ -272,23 +296,17 @@ export const getRuneHolders = async (
     return JSON.parse(cache);
   }
 
-  const resp = await AxiosInstance.get<{
-    code: number;
-    message: string;
-    data: {
-      detail: {
-        address: string;
-        amount: string;
-      }[];
-    };
-  }>(`${BaseUrl(network)}/v1/indexer/runes/${runeId}/holders`, {
-    params: {
-      start: 0,
-      limit: 50,
-    },
+  const data = await tryWithApiKey<{
+    detail: {
+      address: string;
+      amount: string;
+    }[];
+  }>(network, `/v1/indexer/runes/${runeId}/holders`, {
+    start: 0,
+    limit: 50,
   });
 
-  const array = resp.data.data.detail;
+  const array = data.detail;
 
   await RedisInstance.set(
     `unisat:rune:holders:${runeId}`,
